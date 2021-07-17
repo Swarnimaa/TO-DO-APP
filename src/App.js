2333ccc74b6255588c9fda5c0158b864d6e12b79
unknown <swarnimasrivastava2019@gmail.com>
2021-07-18 03:39:54 +0530

    my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [oText, nText] = useState("");
+    const [head, setHead] = useState([]);
+
+    function Addd(event){
+        const a = event.target.value;
+nText(a);
+    };
+
+function Add(event){
+    setHead((prevValue) => {return [...prevValue, oText]});
+    nText(" ");
+}
+
+return(
+    <div>
+        <h1>TO-DO LIST</h1>
+        <input type = "text" placeholder = "Enter text here " onChange = {Addd} value = {oText} />
+        <button type = "button" onClick = {Add} ><span>Add</span></button>
+     <ul> {head.map((a) => {return <><li>{a}</li>
+     {/* <button type = "button" >Delete</button> */}
+     {/* <button type = "button" onClick = {delete((a) => setHead((prevValue, a) => {return prevValue.filter((b, a) => {return b!==a}) }))} >Delete</button> */}
+    </>})} </ul> 
+     </div>
+);
 }
 
 export default App;
